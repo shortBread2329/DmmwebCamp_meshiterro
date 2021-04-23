4eabc136c7ca4b0ac6a0dc178bead9db498e4b22
EC2 Default User <ec2-user@ip-172-31-59-145.ec2.internal>
2021-04-23 16:39:17 +0000

    4章

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '3aceec620f44db918290e8e4591ea40781b84f223ecd818b8df6e8340b60b4fcf0b79088883120cae58afd4928e50f95a481408f18349468677112af2d72c323'
